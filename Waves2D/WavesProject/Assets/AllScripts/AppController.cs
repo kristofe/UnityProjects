4cfdbcbf3530205c5e2b4ff17da980b96180a4dd
Kristofer Schlachter <kristofe72@gmail.com>
2012-09-06 13:23:57 -0400

    Storekit events now listened to in AppController.  Ads are shown based upone persistent flag

--- Waves2D/WavesProject/Assets/AllScripts/AppController.cs
+++ Waves2D/WavesProject/Assets/AllScripts/AppController.cs
@@ -1,20 +1,43 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+
 
 public class AppController : MonoBehaviour {
 	public GameObject infoButton;
 	public InAppPurchaseGUI iapGUI;
 	public GUIMainScreen waveGUI;
+	public bool showAds = true;
 	
-	// Use this for initialization
+	private const string SHOW_ADS = "show_ads";
+
 	void Start () {
-	
-		print("Creating ad banner on bottom");
-		AdBinding.createAdBanner( true );
+	// Listens to all the StoreKit events.  All event listeners MUST be removed before this object is disposed!
+		StoreKitManager.purchaseSuccessful += purchaseSuccessful;
+		StoreKitManager.purchaseCancelled += purchaseCancelled;
+		StoreKitManager.purchaseFailed += purchaseFailed;
+		StoreKitManager.receiptValidationFailed += receiptValidationFailed;
+		StoreKitManager.receiptValidationSuccessful += receiptValidationSuccessful;
+		StoreKitManager.productListReceived += productListReceived;
+		
+		showAds =  PlayerPrefs.GetInt(SHOW_ADS) != 0;
+		if(showAds)
+		{
+			print("Creating ad banner on bottom");
+			AdBinding.createAdBanner( true );
+		}
 	}
 	
 	void OnDisable()
 	{
+		// Remove all the event handlers
+		StoreKitManager.purchaseSuccessful -= purchaseSuccessful;
+		StoreKitManager.purchaseCancelled -= purchaseCancelled;
+		StoreKitManager.purchaseFailed -= purchaseFailed;
+		StoreKitManager.receiptValidationFailed -= receiptValidationFailed;
+		StoreKitManager.receiptValidationSuccessful -= receiptValidationSuccessful;
+		StoreKitManager.productListReceived -= productListReceived;;
+		
 		print("Destroying ad banner");
 		AdBinding.destroyAdBanner();
 	}
@@ -40,9 +63,53 @@ public class AppController : MonoBehaviour {
 			bi.enabled = v;
 	}
 	
-	
-	// Update is called once per frame
-	void Update () {
-	
+	void permanentlyRemoveAds()
+	{
+		PlayerPrefs.SetInt(SHOW_ADS,1);
+		showAds = false;
+		print("Destroying ad banner");
+		AdBinding.destroyAdBanner();
 	}
+	void productListReceived( List<StoreKitProduct> productList )
+	{
+		Debug.Log( "total productsReceived: " + productList.Count );
+		
+		// Do something more useful with the products than printing them to the console
+		foreach( StoreKitProduct product in productList )
+			Debug.Log( product.ToString() + "\n" );
+		
+		permanentlyRemoveAds();
+	}
+	
+	
+	void receiptValidationSuccessful()
+	{
+		Debug.Log( "receipt validation successful" );
+	}
+	
+	
+	void receiptValidationFailed( string error )
+	{
+		Debug.Log( "receipt validation failed with error: " + error );
+	}
+	
+
+	void purchaseFailed( string error )
+	{
+		Debug.Log( "purchase failed with error: " + error );
+	}
+	
+
+	void purchaseCancelled( string error )
+	{
+		Debug.Log( "purchase cancelled with error: " + error );
+	}
+	
+	
+	void purchaseSuccessful( string productIdentifier, string receipt, int quantity )
+	{
+		Debug.Log( "purchased product: " + productIdentifier + ", quantity: " + quantity );
+		permanentlyRemoveAds();
+	}
+	
 }
